22cb12fd2e7b0bada15be17889ac8f2231c57029
Forrest Benton <elliottbenton@gmail.com>
2018-06-26 17:22:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,23 @@
 class Dog
+  def initialize(dog_name,breed_name)
+    @name = dog_name
+    @breed = breed_name
+  end
+  
   def name 
-    puts name
+    @name
   end 
   
-  def name=(dog_name)
-    @name = dog_name
+  def name=(breed_name)
+    @name = breed_name
   end
   
   def breed 
+    @breed
   end 
   
   def breed=(breed_name)
+    @breed = breed_name
+  end
   
 end
